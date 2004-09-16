c7f3fe2123c52f06580d02cbc8a4162ecd5c3b7c
Oren Miller <oren@quickfixengine.org>
2004-09-16 17:54:19 +0000

    *** empty log message ***

--- fixtohtml.sh
+++ fixtohtml.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for XML in FIX??_*.xml
-do
-    ./trimxml.rb $XML > $XML.tmp
-    mv $XML.tmp $XML
-done
+#for XML in FIX??_*.xml
+#do
+#    ./trimxml.rb $XML > $XML.tmp
+#    mv $XML.tmp $XML
+#done
 
 for XML in FIX*.xml 
 do
-    HTML=`echo $XML | sed 's/xml/html/'`
-    xsltproc fixtohtml.xsl $XML > $HTML
+  echo $XML 
+  HTML=`echo $XML | sed 's/xml/html/'`
+  xsltproc fixtohtml.xsl $XML > $HTML
 done
